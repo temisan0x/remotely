c5e9a009d713d26f6d25535629754770fbd14510
Heytemisan <temyicy@gmail.com>
2023-02-14 09:01:35 +0100

    chore: bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./Layout/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      purple: "#442AF9",
+      white: "#FFFFFF"
+    },
     extend: {},
   },
   variants: {
