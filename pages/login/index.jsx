2594680d048c149f318aed804c6a1941dfe66e6f
Heytemisan <temyicy@gmail.com>
2023-02-23 18:16:15 +0100

    feat: created login page

---
+++ pages/login/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Login() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Remotely login page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+    <div>
+      Hello
+    </div>
+
+    </div>
+  )
+}
