a683e38c158e584e8131bb5fb9a3f47de668d3f6
Heytemisan <temyicy@gmail.com>
2023-02-26 15:25:09 +0100

    feat: created login page

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -11,25 +11,19 @@ export default function Login() {
       </Head>
 
       <section className="justify-center text-center py-24 flex-col flex item-center">
-        <h1>Welcome To Remotely</h1>
-        <p>
+        <h1 className="font-medium text-[48px] text-[#000000]">Welcome To Remotely</h1>
+        <p className="text-[24px] w-[50%] m-auto">
           Learn from the world-renowed experts with interactive projects and
           case studies
         </p>
-        <h4>Choose Login Type</h4>
-        <div>
-        <Link
-          href="/"
-          className="bg-purple text-white"
-        >
-          <span className="mx-1 text-white text-lg font-normal">Student</span>
-        </Link>
-        <Link
-          href="/"
-          className="bg-purple text-white"
-        >
-          <span className="mx-1 text-white text-lg font-normal">Student</span>
-        </Link>
+        <p className="text-[39px] py-10">Choose Login Type</p>
+        <div className="flex flex-col m-auto">
+          <Link href="/" className="bg-purple text-white p-5 mt-2">
+            <span className="mx-1 text-white text-lg font-normal text-2xl">Student</span>
+          </Link>
+          <Link href="/" className="bg-purple text-white py-5 px-28  my-5">
+            <span className="mx-1 text-white text-lg font-normal text-2xl">Coach</span>
+          </Link>
         </div>
       </section>
     </div>
